25ce84dc9427d156d4cefd6774b1d209ec7c5b08
arjunarj <nagarjunan66@gmail.com>
2022-09-08 03:15:29 +0530

    Added Info card with Navigation and Dialer

--- lib/pages/map_display.dart
+++ lib/pages/map_display.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 //import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:geolocator/geolocator.dart';
@@ -21,20 +23,21 @@ class MapDisp extends StatefulWidget {
 
 class _MapDispState extends State<MapDisp> {
   List<Marker> markers = [];
-
+  late List pos;
+  late bool isAvailable;
+  TextStyle hospitalStyle = GoogleFonts.poppins(
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+    color: Colors.black,
+  );
   void initState() {
     super.initState();
     _determinePosition();
-    markers.add(Marker(
-        markerId: MarkerId('mymarker'),
-        draggable: false,
-        onTap: () => print('tapped'),
-        position: LatLng(0, 0),
-        infoWindow: InfoWindow()));
   }
 
   bool _isHospitalSelected = false;
   final user = FirebaseAuth.instance.currentUser!;
+
   //Map<String, dynamic>?
   Map<MarkerId, Marker> _markers = <MarkerId, Marker>{};
   initMarker(request) {
@@ -43,21 +46,30 @@ class _MapDispState extends State<MapDisp> {
     bool isPresent = request['isPresent'];
     final MarkerId markerId = MarkerId(markerIdVal);
     final Marker marker = Marker(
-        icon: isPresent
-            ? BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen)
-            : BitmapDescriptor.defaultMarker,
-        markerId: markerId,
-        position: LatLng(p[0], p[1]),
-        infoWindow: InfoWindow(
-          title: request['name'],
-          onTap: () {
-            launchUrl(Uri.parse("google.navigation:q=${p[0]},${p[1]}"));
-            setState(() {
-              _isHospitalSelected = !_isHospitalSelected;
-              _hospitalSelected = markerIdVal;
-            });
-          },
-        ));
+      onTap: () {
+        setState(() {
+          _isHospitalSelected = !_isHospitalSelected;
+          _hospitalSelected = markerIdVal;
+          pos = p;
+          isAvailable = isPresent;
+        });
+      },
+      icon: isPresent
+          ? BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen)
+          : BitmapDescriptor.defaultMarker,
+      markerId: markerId,
+      position: LatLng(p[0], p[1]),
+      // infoWindow: InfoWindow(
+      //   title: request['name'],
+      //   onTap: () {
+      //     launchUrl(Uri.parse("google.navigation:q=${p[0]},${p[1]}"));
+      //     setState(() {
+      //       _isHospitalSelected = !_isHospitalSelected;
+      //       _hospitalSelected = markerIdVal;
+      //     });
+      //   },
+      // ),
+    );
     setState(() {
       _markers[markerId] = marker;
     });
@@ -121,9 +133,6 @@ class _MapDispState extends State<MapDisp> {
         mapType: MapType.normal,
         myLocationButtonEnabled: false,
         initialCameraPosition: CameraPosition(
-          // target: position == null
-          //     ? LatLng(0.0, 0.0)
-          //     : LatLng(position!.latitude, position!.longitude),
           target: LatLng(143.2, 154.78),
           zoom: 14.4746,
         ),
@@ -131,51 +140,207 @@ class _MapDispState extends State<MapDisp> {
           _controller.complete(controller);
         },
       ),
-      if (_isHospitalSelected)
-        Align(
-            alignment: Alignment.center,
-            child: Card(
-                color: Colors.indigoAccent,
-                child: Text("Selected ${_hospitalSelected}"))),
       Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width - 50,
-          child: FloatingActionButton(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              backgroundColor: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: GestureDetector(
+            child: Container(
+              height: 55,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.green[800]),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Icon(
-                      FlutterRemix.user_location_line,
-                      color: Colors.black,
+                      FlutterRemix.hospital_line,
+                      color: Colors.white,
                     ),
                   ),
                   Text(
-                    "Tap to get Data near your location",
-                    style: TextStyle(color: Colors.black),
+                    "Hospitals near me",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   )
                 ],
               ),
-              onPressed: () async {
-                Position position = await _determinePosition();
-                final GoogleMapController controller = await _controller.future;
-                await _populateMarks();
-                //print(_hospitalData);
-                controller.animateCamera(
-                    CameraUpdate.newCameraPosition(CameraPosition(
-                        target: LatLng(
-                          position.latitude,
-                          position.longitude,
-                        ),
-                        zoom: 14.4746)));
-              }),
-        ),
+            ),
+            onTap: () async {
+              Position position = await _determinePosition();
+              final GoogleMapController controller = await _controller.future;
+              await _populateMarks();
+              //print(_hospitalData);
+              controller
+                  .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                      target: LatLng(
+                        position.latitude,
+                        position.longitude,
+                      ),
+                      zoom: 14.4746)));
+            }),
       ),
+      if (_isHospitalSelected)
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${_hospitalSelected}",
+                        style: hospitalStyle,
+                      ),
+                      SizedBox(height: 10),
+                      isAvailable
+                          ? Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Emergency available',
+                                  style: TextStyle(color: Colors.green),
+                                ),
+                                Spacer(),
+                                Icon(
+                                  FlutterRemix.check_fill,
+                                  color: Colors.green,
+                                ),
+                                Spacer(flex: 6),
+                                IconButton(
+                                  onPressed: () {
+                                    launchUrl(Uri.parse("tel:7397665588"));
+                                  },
+                                  icon: Icon(
+                                    FlutterRemix.phone_fill,
+                                    size: 36,
+                                  ),
+                                ),
+                                Spacer(),
+                                IconButton(
+                                  onPressed: () {
+                                    launchUrl(Uri.parse(
+                                        "google.navigation:q=${pos[0]},${pos[1]}"));
+                                  },
+                                  icon: Icon(
+                                    FlutterRemix.compass_discover_fill,
+                                    size: 36,
+                                  ),
+                                ),
+                                // Spacer(flex:1),
+                              ],
+                            )
+                          : Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'Emergency unavailable',
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                                Spacer(),
+                                Icon(
+                                  FlutterRemix.close_fill,
+                                  color: Colors.red,
+                                ),
+                                Spacer(flex: 6),
+                                IconButton(
+                                  onPressed: () {
+                                    launchUrl(Uri.parse("tel:7397665588"));
+                                  },
+                                  icon: Icon(
+                                    FlutterRemix.phone_fill,
+                                    size: 36,
+                                  ),
+                                ),
+
+                                Spacer(),
+                                IconButton(
+                                  onPressed: () {
+                                    launchUrl(Uri.parse(
+                                        "google.navigation:q=${pos[0]},${pos[1]}"));
+                                  },
+                                  icon: Icon(
+                                    FlutterRemix.compass_discover_fill,
+                                    size: 36,
+                                  ),
+                                ),
+                                // Spacer(flex:1),
+                              ],
+                            ),
+                      // Text('Specialities:'),
+                      // Expanded(
+                      //   child: GridView.count(
+                      //     childAspectRatio: 2,
+                      //     crossAxisCount: 2,
+                      //     children: [
+                      //       Padding(
+                      //         padding: const EdgeInsets.all(8.0),
+                      //         child: Container(
+                      //           height: 50,
+                      //           decoration: BoxDecoration(
+                      //               borderRadius: BorderRadius.circular(12),
+                      //               color: Colors.green[100]),
+                      //           child: Padding(
+                      //             padding: const EdgeInsets.all(8.0),
+                      //             child: Row(
+                      //               mainAxisAlignment:
+                      //                   MainAxisAlignment.spaceBetween,
+                      //               children: [
+                      //                 Text('ICU',
+                      //                     style:
+                      //                         TextStyle(color: Colors.green)),
+                      //                 Icon(FlutterRemix.add_box_line,
+                      //                     color: Colors.green)
+                      //               ],
+                      //             ),
+                      //           ),
+                      //         ),
+                      //       ),
+                      //       Padding(
+                      //         padding: const EdgeInsets.all(8.0),
+                      //         child: Container(
+                      //           height: 50,
+                      //           decoration: BoxDecoration(
+                      //               borderRadius: BorderRadius.circular(12),
+                      //               color: Colors.red[100]),
+                      //           child: Padding(
+                      //             padding: const EdgeInsets.all(8.0),
+                      //             child: Row(
+                      //               mainAxisAlignment:
+                      //                   MainAxisAlignment.spaceBetween,
+                      //               children: [
+                      //                 Text('Cardiologist',
+                      //                     style: TextStyle(color: Colors.red)),
+                      //                 Icon(FlutterRemix.heart_pulse_line,
+                      //                     color: Colors.red)
+                      //               ],
+                      //             ),
+                      //           ),
+                      //         ),
+                      //       )
+                      //     ],
+                      //   ),
+                      // )
+                    ],
+                  ),
+                ),
+              ),
+            )),
     ]);
   }
 }
