2de7bab278f50cf3a5618fb1ca5c34b6ff72e13d
RS <sanjaykrishna584@gmail.com>
2022-09-07 15:37:06 +0530

    Added iOS compatibility and markers

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,19 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDmv5vOLW6nBzfMQAl6PVvhgJfBh8nRP6k")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+// @UIApplicationMain
+// @objc class AppDelegate: FlutterAppDelegate {
+//   override func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//   ) -> Bool {
+//     GeneratedPluginRegistrant.register(with: self)
+//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+// }
--- lib/pages/map_display.dart
+++ lib/pages/map_display.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_remix/flutter_remix.dart';
 
 class MapDisp extends StatefulWidget {
@@ -17,6 +20,36 @@ class _MapDispState extends State<MapDisp> {
   void initState() {
     super.initState();
     _determinePosition();
+     }
+  final user = FirebaseAuth.instance.currentUser!;
+  //Map<String, dynamic>? 
+  Map<MarkerId, Marker> _markers = <MarkerId, Marker>{};
+    initMarker(request) {
+    var p = request['lat'];
+    var markerIdVal = request['name'];
+    final MarkerId markerId = MarkerId(markerIdVal);
+    final Marker marker = Marker(
+        markerId: markerId,
+        position:
+            LatLng(p[0], p[1]),
+        infoWindow: InfoWindow(
+          title: request['name'],
+        ));
+    setState(() {
+      _markers[markerId] = marker;
+
+    });
+  }
+   Future _populateMarks() async{
+    //initMarker({'name':'Example','lat':[10.851274920328036, 77.0527618629687]});
+    FirebaseFirestore.instance.collection('hospitals').get().then((QuerySnapshot doc){
+      if(doc.docs.isNotEmpty){
+        print(doc.docs[0].data);
+        for(int i = 0; i < doc.docs.length; i++){
+          initMarker(doc.docs[i].data);
+        }
+      }
+    });
   }
 
   Completer<GoogleMapController> _controller = Completer();
@@ -49,16 +82,19 @@ class _MapDispState extends State<MapDisp> {
     return Scaffold(
       body: GoogleMap(
         zoomControlsEnabled: false,
+        markers: Set.of(_markers.values),
         mapType: MapType.normal,
+         myLocationButtonEnabled: false,
         initialCameraPosition: CameraPosition(
           // target: position == null
           //     ? LatLng(0.0, 0.0)
           //     : LatLng(position!.latitude, position!.longitude),
-          target: LatLng(0.0, 0.0),
+          target: LatLng(143.2, 154.78),
           zoom: 14.4746,
         ),
-        onMapCreated: (GoogleMapController controller) {
+        onMapCreated: (GoogleMapController controller) async{
           _controller.complete(controller);
+          await _populateMarks();
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -73,6 +109,9 @@ class _MapDispState extends State<MapDisp> {
           onPressed: () async {
             Position position = await _determinePosition();
             final GoogleMapController controller = await _controller.future;
+            await _populateMarks();
+            print(_markers);
+            //print(_hospitalData);
             controller
                 .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
                     target: LatLng(
